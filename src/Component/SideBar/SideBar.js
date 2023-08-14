a9ebadcdd03cc4e839f86bdafea315a621c02d47
mohdsadique012 <mohdsadique012000@gmail.com>
2023-08-14 22:03:16 +0530

    second

--- src/Component/SideBar/SideBar.js
+++ src/Component/SideBar/SideBar.js
@@ -1,36 +1,64 @@
-import React, { useContext, useEffect, useState } from 'react'
-import "./SideBar.css"
-import { Link } from 'react-router-dom'
-import { AppContext, useProfileGlobal } from '../../Context/ProfileContext';
+import React, { useContext, useEffect, useState } from "react";
+import "./SideBar.css";
+import { Link } from "react-router-dom";
+import { AppContext, useProfileGlobal } from "../../Context/ProfileContext";
 function SideBar() {
-    let Data = useContext(AppContext);
-    const {getStatus} = useProfileGlobal();
-  
-   // using useefect for getting single data of user 
-const [status ,setStatus]=useState(true)
-const [profile ,setProfile]=useState("Profile")
+  let Data = useContext(AppContext);
+  const { getStatus } = useProfileGlobal();
 
-   useEffect(()=>{
-    getStatus(status,profile)
-    console.log(profile,"mnmnmnmn")
-   },[status,profile])
-  
-  
-  
+  // using useefect for getting single data of user
+  const [status, setStatus] = useState(true);
+  const [profile, setProfile] = useState("Profile");
+
+  useEffect(() => {
+    getStatus(status, profile);
+    console.log(profile, "mnmnmnmn");
+  }, [status, profile]);
 
   return (
     <>
-    <div className="sidebar">
-    <div >
-    <p  onClick={()=>{setStatus(true);setProfile("Profile")}} className='sidebar-p'>Profile <span>{">"}</span></p>
-    <p  onClick={()=>{setStatus(false);setProfile("Post")}}  className='sidebar-p'>Post <span>{">"}</span></p>
-    <p  onClick={()=>{setStatus(false);setProfile("Gallery")}}  className='sidebar-p'>Gallery <span>{">"}</span></p>
-    <p  onClick={()=>{setStatus(false);setProfile("ToDo")}}  className='sidebar-p'>ToDo <span>{">"}</span></p>
-    </div>
-    
-  </div>
+      <div className="sidebar">
+        <div>
+          <p
+            onClick={() => {
+              setStatus(true);
+              setProfile("Profile");
+            }}
+            className="sidebar-p"
+          >
+            Profile 
+          </p>
+          <p
+            onClick={() => {
+              setStatus(false);
+              setProfile("Post");
+            }}
+            className="sidebar-p"
+          >
+            Post 
+          </p>
+          <p
+            onClick={() => {
+              setStatus(false);
+              setProfile("Gallery");
+            }}
+            className="sidebar-p"
+          >
+            Gallery 
+          </p>
+          <p
+            onClick={() => {
+              setStatus(false);
+              setProfile("ToDo");
+            }}
+            className="sidebar-p"
+          >
+            ToDo 
+          </p>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default SideBar
+export default SideBar;
